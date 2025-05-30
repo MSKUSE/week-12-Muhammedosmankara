4f115a31a0477cda0f0b5138a5374113a60294d1
Muhammedosmankara <muhammedosmankara@posta.mu.edu.tr>
2025-05-30 12:03:04 +0300

    week12

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args){
+
+        StackLinkedList<Object> stack = new StackLinkedList<>();
+        stack.push(3);
+        stack.push(45);
+        stack.push(3.4);
+        stack.push("This is a test");
+        stack.displayStack();
+
+        //stack.push("This is a test");
+
+
+        /*
+
+        ArrayList<String> stringList = new ArrayList();
+        stringList.add("String");
+        stringList.add("Ali");
+        System.out.println(stringList);
+
+        HashMap<String, Double> grades = new HashMap();
+        grades.put("Ali", 10.0);
+        grades.put("Pelin" , 75.4);
+        grades.put("Koray", 4.5);
+        System.out.println(grades);
+        grades.put("Ali", 100.0);
+        System.out.println(grades); */
+
+
+
+
+
+    }
+}
---
+++ src/StackItem.java
@@ -0,0 +1,32 @@
+public class StackItem<T> {
+    private T data;
+    private StackItem<T> next;
+
+    public StackItem(T data) {
+        this.data = data;
+    }
+
+    public StackItem<T> getNext() {
+        return next;
+    }
+
+    public void setNext(StackItem next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "StackItem{" +
+                "data=" + data +
+
+                '}';
+    }
+}
